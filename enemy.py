9f4d1575ae7ee62711be99c773693264a8a05198
Andrey Varvaryuk <andrey.varvaryuk@gmail.com>
2023-06-01 11:03:10 +0300

    basic enemy stuff

--- enemy.py
+++ enemy.py
@@ -1,13 +1,32 @@
 from settings import *
 
 
+class TankGroup:
+    def __init__(self, tanks):
+        self.tanks = tanks
+
+    def update(self):
+        pass
+        # check if any of the tanks died, if so remove them from the list
+        # basically call "update" of every tank
+
+
 class Tank:
     def __init__(self, pos, max_health, initial_rotation):
         self.pos = pos
+
         self.max_health = max_health
         self.health = self.max_health
+
         self.rotation = initial_rotation
         self.turret_rotation = initial_rotation
 
+        self.dead = False
 
+        self.bullet_type = 0
 
+    def draw(self):
+        print("you forgot to implement this feature in the child class")
+
+    def move(self):
+        print("you forgot to implement this feature in the child class")
