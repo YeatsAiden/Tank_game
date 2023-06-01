86f22ff7c4abfb1e2240d6c58bd48e75fdce3f9b
Andrey Varvaryuk <andrey.varvaryuk@gmail.com>
2023-06-01 10:31:08 +0300

    STUPID GIT DOESNT ALLOW ME TO DO ANYTHING WITHOUT OMMITING STUFF AAAAHHHHH

--- enemy.py
+++ enemy.py
@@ -2,8 +2,12 @@ from settings import *
 
 
 class Tank:
-    def __init__(self, pos, max_health):
+    def __init__(self, pos, max_health, initial_rotation):
         self.pos = pos
         self.max_health = max_health
         self.health = self.max_health
-        self.turret_angle
+        self.rotation = initial_rotation
+        self.turret_rotation = initial_rotation
+
+
+
